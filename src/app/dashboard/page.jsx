79276b59e78df383fb1d4abc1b18e17bacb8fa9b
Miguel05HB <76132931@certus.edu.pe>
2024-01-31 20:20:10 -0500

    Cambios-new

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,7 +5,7 @@ function DashboardPage() {
   return (
     <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <div className="text-center">
-        <h1 className="text-5xl font-bold">Dashboard</h1>
+        <h1 className="text-5xl font-bold">Dashboard!</h1>
         <button
           onClick={() => signOut()}
           className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
